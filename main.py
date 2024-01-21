f492bfcddf26ba247980ee6b3c32ba1ba37d495d
lalka1313 <137827932+lalka1313@users.noreply.github.com>
2024-01-21 17:55:07 +0100

    Firs commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Guess a number Game
+
+import random
+
+computer_number = random.randint(1, 100)
+
+while True:
+    player_input = input("Guess the nimber (1-100): ")
+
+    if not player_input.isdigit():
+        print("Invalid input. Try again ...")
+        continue
+    player_number = int(player_input)
+
+    if player_number == computer_number:
+        print("You guess it!")
+        break
+    elif player_number > computer_number:
+        print("Too High!")
+    elif player_number < computer_number:
+        print("Too Low!")
